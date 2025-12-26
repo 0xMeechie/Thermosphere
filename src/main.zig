b4c362e19a8df698aa20c248c892dfdc5c068888
Christopher Ritter <60591591+fdaygon@users.noreply.github.com>
2025-12-25 23:37:14 -0600

    build clean up

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
-const Thermosphere = @import("Thermosphere");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try Thermosphere.bufferedPrint();
 }
 
 test "simple test" {
